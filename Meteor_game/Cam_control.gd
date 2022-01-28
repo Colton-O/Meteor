fcef09b0b23b55d5119667979a4d09021e8f4262
Colton Ondewrwater <33445050+Colton-O@users.noreply.github.com>
2022-01-28 23:39:09 +0800

    no message

--- Meteor_game/Cam_control.gd
+++ Meteor_game/Cam_control.gd
@@ -7,12 +7,14 @@ var z = 7
 var target
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target = get_node("../KinematicBody")
+	target = get_node("../meteor")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(_delta):
 	var t_pos = target.translation
+	var m_vel = target.velocity
+	
 	camPos = Vector3(t_pos.x + x, t_pos.y + y, t_pos.z + z)
 
 	look_at_from_position(camPos, t_pos, Vector3.UP)
