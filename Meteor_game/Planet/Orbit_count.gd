724f6ac59a289122d16a976353148b22451b95c8
Colton Ondewrwater <33445050+Colton-O@users.noreply.github.com>
2022-01-29 19:09:19 +0800

    Added orbit score

--- Meteor_game/Planet/Orbit_count.gd
+++ Meteor_game/Planet/Orbit_count.gd
@@ -1,11 +1,17 @@
 extends Area
 var init = false
+var score = 0
+var score_text
 func _ready():
 	connect("body_entered", self, "on_body_enter")
+	score_text = get_node("../Score")
+	score_text.text = "orbits: " + str(score)
 	
 func on_body_enter(body):
 	if init:
-		print ("yes")
+		
+		score += 1
+		score_text.text = "orbits: " + str(score)
 	else:
 		init = true
 
