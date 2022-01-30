405585f90897a871ca8a42eefed340e1e0fad51e
MilesJHansen <milesjhansen@gmail.com>
2022-01-30 11:07:16 +0800

    camera zooms on speed

--- Meteor_game/Meteaor/Cam_control.gd
+++ Meteor_game/Meteaor/Cam_control.gd
@@ -1,6 +1,7 @@
 extends Camera
 
 var camPos: Vector3 = Vector3(0,0,0)
+var camOffset: Vector3 = Vector3(10,5,-10)
 var x = 10
 var y = 5
 var z = -10
@@ -14,7 +15,7 @@ func _ready():
 func _process(_delta):
 	var t_pos = target.translation
 	# var m_vel = target.velocity
-	camPos = Vector3(t_pos.x + x, t_pos.y + y, t_pos.z + z)
+	camPos = t_pos + camOffset * (1 + target.velocity.length())  #Vector3(t_pos.x + x, t_pos.y + y, t_pos.z + z)
 	
 
 	look_at_from_position(camPos, t_pos, Vector3.UP)
